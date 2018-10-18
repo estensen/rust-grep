16e29e2a48003dfc90b44f6748b91eafdb3c8933
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-18 22:13:54 +0200

    Add simple grep

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate regex;
+#[macro_use]
+extern crate clap;
+
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use regex::Regex;
+use clap::App;
+
+fn read_lines(pattern: String, filename: String, count: bool) {
+    let f = File::open(filename).expect("file not found");
+    let buffered = BufReader::new(f);
+
+    let re = Regex::new(&pattern).unwrap();
+    let mut matched_lines = 0;
+
+    for line in buffered.lines() {
+        let line_string = line.expect("coutld not read line");
+        if re.is_match(&line_string) {
+            if count {
+                matched_lines += 1;
+            } else {
+                println!("{}", &line_string);
+            }
+        }
+    }
+    if count {
+        println!("{}", matched_lines);
+    }
+}    
+
+fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let pattern = matches.value_of("pattern").unwrap().to_string();
+    let filename = matches.value_of("filename").unwrap().to_string();
+    let count = matches.is_present("count"); 
+
+    read_lines(pattern, filename, count);
+}
+
